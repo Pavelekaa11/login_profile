054a9d2b686f0e30f9eb31def030f63de833cf8d
Pavelekaa <edwinpaveleka@gmail.com>
2025-04-27 10:56:28 +0700

    first commit

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'profile_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool sembunyikanPassword = true; // buat kontrol tampilan password
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +20,26 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(decoration: InputDecoration(labelText: 'Email')),
             TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            TextField(
+              obscureText: sembunyikanPassword,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    sembunyikanPassword ? Icons.visibility_off : Icons.visibility,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      sembunyikanPassword = !sembunyikanPassword;
+                    });
+                  },
+                ),
+              ),
             ),
             SizedBox(height: 20),
             ElevatedButton(
